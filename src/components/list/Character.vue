1a2d881d83b5077099893c31120a6b59e5db7dc3
iRinwind <illusionarydominance009@gmail.com>
2017-03-07 16:07:52 +0800

    Cardlist Work

--- src/components/list/Character.vue
+++ src/components/list/Character.vue
@@ -1,21 +1,28 @@
 <template>
   <div class="row-fluid show-grid">
+      
     <!-- Start of post image -->
     <div class="span5" align="center">
-       <h2 class="title" style="text-align:center">{{ card.title }} {{ card.name }}</h2>
-
       <img class="alignleft" :src="require('assets/card-images/' + card.sku + '.jpg')"
       width="300" height="409" align="middle" />
     </div>
     <div class="span7" align="center">
-      <div class="btn btn-primary disabled" style="width:14%">{{ card.sku }}</div>
-      <div class="btn btn-inverse disabled" style="width:67%">画师：{{ card.illustrator }} | 版本：{{ card.version }}</div>
-      <div class="btn btn-danger disabled"
-      style="width:10%">{{ card.rarity }}</div>
+        <div class="btn btn-title" style="width:98.5%" >
+        <span class="badge badge-sku" style="float:left">{{ card.sku }}
+        </span> 「{{ card.title }}」{{ card.name }}
+        
+        <span :class="'badge badge-' + card.rarity" style="float:right">{{ card.rarity }}</span>
+
+      </div>
+        <div :class="'progress progress-' + card.rarity + ' progress-striped active'">
+        <div :class="'bar bar-' + card.rarity">
+        画师：{{ card.illustrator }} &nbsp &nbsp | &nbsp &nbsp 版本：{{ card.version }}</div>
+      </div>
+
       <table class="table table-bordered table-striped">
         <tbody v-for="s in card.skills" class="nav nav-list">
           <tr>
-            <td><span class="badge badge-inverse">{{ s.type }}</span>
+            <td><span :class="'badge badge-'+ s.type">{{ s.type }}</span>
             {{ s.name }}</td>
           </tr>
           <tr>
