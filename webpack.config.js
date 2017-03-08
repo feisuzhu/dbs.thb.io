99ddbf3646d64d8660763ef6d8d4a8ad2148ac6f
Proton <feisuzhu@163.com>
2017-03-08 23:38:23 +0800

    Enable optimizers

--- webpack.config.js
+++ webpack.config.js
@@ -99,7 +99,7 @@ if (process.env.NODE_ENV === 'production') {
         NODE_ENV: '"production"'
       }
     }),
-    /*
+    //*
     new webpack.optimize.UglifyJsPlugin({
       sourceMap: true,
       compress: {
