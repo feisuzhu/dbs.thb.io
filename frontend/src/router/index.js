8baefe83e4bb0df9f30e4283f6dae3f0c5adc15a
Proton <feisuzhu@163.com>
2023-04-22 18:32:38 +0800

    fix(frontend): Workaround weird issue (vue-router not working in production mode)

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -3,28 +3,20 @@ import { createRouter, createWebHistory } from 'vue-router'
 const router = createRouter({
   history: createWebHistory(''),
   routes: [
-    {path: '/',                   component: import('@/views/landing/Landing.vue')},
-    {path: '/works',              component: import('@/views/Works.vue')},
-    {path: '/:page(rules)',       component: import('@/views/rules/RulesPage.vue')},
-    {path: '/:page(vocabulary)',  component: import('@/views/rules/RulesPage.vue')},
-    {path: '/changelog',          component: import('@/views/rules/ChangeLog.vue')},
-    {path: '/articles',           component: import('@/views/article/ArticleList.vue')},
-    {path: '/articles/:category', component: import('@/views/article/ArticleList.vue')},
-    {path: '/articles/:category/:slug', component: import('@/views/article/Article.vue')},
+    {path: '/',                   component: () => import('@/views/landing/Landing.vue')},
+    {path: '/works',              component: () => import('@/views/Works.vue')},
+    {path: '/:page(rules)',       component: () => import('@/views/rules/RulesPage.vue')},
+    {path: '/:page(vocabulary)',  component: () => import('@/views/rules/RulesPage.vue')},
+    {path: '/changelog',          component: () => import('@/views/rules/ChangeLog.vue')},
+    {path: '/articles',           component: () => import('@/views/article/ArticleList.vue')},
+    {path: '/articles/:category', component: () => import('@/views/article/ArticleList.vue')},
+    {path: '/articles/:category/:slug', component: () => import('@/views/article/Article.vue')},
 
-    {path: '/community',          component: import('@/views/community/Community.vue')},
-    {path: '/shop',               component: import('@/views/community/Shop.vue')},
+    {path: '/community',          component: () => import('@/views/community/Community.vue')},
+    {path: '/shop',               component: () => import('@/views/community/Shop.vue')},
 
-    // {path: '/about',    component: import('@/views/About.vue')},
-    // {path: '/blogs',    component: import('@/views/BlogList.vue')},
-    // {path: '/blog/:id', component: import('@/views/Blog.vue')},
-    // {path: '/rules',    component: import('@/views/Rules.vue')},
-    // {path: '/story',    component: import('@/views/Story.vue')},
+    {path: '/:sku',               component: () => import('@/views/Collection.vue')},
 
-    {path: '/:sku',         component: import('@/views/Collection.vue')},
-
-    // {path: '/:col/:sku',    component: import('@/views/card/Card.vue'), name: 'card'},
-    //
     { path: '/:pathMatch(.*)', redirect: '/' }
   ]
 })
